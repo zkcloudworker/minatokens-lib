cf8c46fd02260df76be242aefd62880e21efdc97
DFST <mk@dfst.io>
2024-12-22 00:33:45 +0300

    0.11.0

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,35 +1,35 @@
-const { execSync } = require("child_process");
-const readline = require("readline");
+import { execSync } from "child_process";
+import readline from "readline";
 
 (async () => {
   // Run the build command
   execSync("npm run build", { stdio: "inherit" });
 
   // Prompt the user for the OTP
-  const rl = readline.createInterface({
+  const rl: readline.Interface = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
 
-  const otp = await new Promise((resolve) => {
-    rl.question("Enter OTP: ", (answer) => {
+  const otp: string = await new Promise((resolve) => {
+    rl.question("Enter OTP: ", (answer: string) => {
       rl.close();
       resolve(answer.trim());
     });
   });
 
   // List of your packages
-  const packages = [
+  const packages: string[] = [
     "@minatokens/api",
     "@minatokens/storage",
     "@minatokens/token",
     "@minatokens/upgradable",
     "@minatokens/nft",
-    "@minatokens/upgradable-v1",
+    "@minatokens/abi",
   ];
 
   // Run the release command for each package
-  packages.forEach((pkg) => {
+  packages.forEach((pkg: string) => {
     execSync(`npm run release -w ${pkg}`, {
       env: { ...process.env, NPM_CONFIG_OTP: otp },
       stdio: "inherit",
